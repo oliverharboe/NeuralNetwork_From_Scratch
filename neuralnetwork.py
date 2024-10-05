d6e474289dbd2de1a1037fd6c417168bb931a406
Oliver Harboe <oli.harboe@gmail.com>
2024-10-05 15:10:22 +0200

    forward prop

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,12 +2,18 @@ import numpy as np
 
 class NerualNetwork:
     def __init__(self) -> None:
+        '''
+        Initalizere parameters (weights and biases)
+        '''
         self.w1 = np.random.randn(16,784)
         self.b1 = np.random.randn(16,1)
         self.w2 = np.random.randn(16,10)
         self.b2 = np.random.randn(10,1)
     
     def forward_prop(self,X):
+        '''
+        Forward propagation
+        '''
         z1 = np.dot(self.w1,X.T) + self.b1
         a1 = relu(z1)
         z2 = np.dot(self.w2,a1) + self.b2
@@ -21,8 +27,10 @@ class NerualNetwork:
 
 
 def softmax(x):
+    # Definere Softmax funktionen
     return np.exp(x) / np.sum(np.exp(x),axis=0)
 
 
 def relu(x):
+    # Definere ReLu funktionen
     return np.maximum(0,x)

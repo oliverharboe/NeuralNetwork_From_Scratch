66c30318db9a66ec78a02d674108a430ac2f21e7
Oliver Harboe <oli.harboe@gmail.com>
2024-11-03 18:39:55 +0100

    softmax function changed

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -7,36 +7,56 @@ class NerualNetwork:
         '''
         self.w1 = np.random.randn(16,784)
         self.b1 = np.random.randn(16,1)
-        self.w2 = np.random.randn(16,10)
+        self.w2 = np.random.randn(10,16)
         self.b2 = np.random.randn(10,1)
     
-    def predict(self,X:np.ndarray) -> np.ndarray:
+    def forwardProp(self,X:np.ndarray) -> np.ndarray:
         '''
         Forward propagation
         '''
         z1 = np.dot(self.w1,X.T) + self.b1
-        a1 = relu(z1)
+        a1 = ReLU(z1)
         z2 = np.dot(self.w2,a1) + self.b2
         a2 = softmax(z2)
-        return a2
+
+        return z1,a1,z2,a2
     
-    def fit(self,X:np.ndarray,y:np.ndarray) -> None:
+    def backProp(self,X:np.ndarray,y:np.ndarray) -> None:
         """
         Backpropagation
         """
-        pass
-
-def relu_m(x:float) -> float:
-    # differentieret relu funktion
-    return np.where(x > 0, 1,0)
 
     
 
+
+    def gradientDescent(self,X:np.ndarray,y:np.ndarray,epochs:int,learning_rate:float) -> None:
+        '''
+        Gradient Descent
+        optimizing the parameters
+        '''
+        for epoch in range(epochs):
+            pass
+            
+    def prediction(self,X:np.ndarray) -> np.ndarray:
+        '''
+        changes from a onehot encoded vector to a number
+        '''
+        vector = self.forwardProp(X)
+        pre = np.argmax(vector,axis=0)
+        return pre
+
+def ReLU_m(x:float) -> float:
+    # differentieret relu funktion
+    return np.where(x > 0, 1,0)
+
+
 def softmax(x:float) -> float:
-    # Definere Softmax funktionen
-    return np.exp(x) / np.sum(np.exp(x),axis=0)
+    # Softmax funktionen 
+    # funktionen er en variant der gør at 
+    
+    y = np.exp(x-np.max(x)) / np.sum(np.exp(x-np.max(x)),axis=0)
+    return y
 
-
-def relu(x:float) -> float:
+def ReLU(x:float) -> float:
     # Definere ReLu funktionen
     return np.maximum(0,x)
